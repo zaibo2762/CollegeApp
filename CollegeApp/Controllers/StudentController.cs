1fe1f549075c24654b9126c08a4fabbdeb69a98e
zaibo2762 <77626531+zaibo2762@users.noreply.github.com>
2024-04-18 14:42:39 +0500

    Add project files.

---
+++ CollegeApp/Controllers/StudentController.cs
@@ -0,0 +1,81 @@
+﻿using CollegeApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CollegeApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        [Route("All" , Name ="GetAllStudents")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public  ActionResult<IEnumerable<Student>> GetStudents()
+        {
+            return Ok(CollegeRepository.Students);
+        }
+
+        [HttpGet]
+        [Route("{id:int}" , Name ="GetStudentById")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public ActionResult< Student> GetStudentbyid(int id)
+        {
+            if (id <= 0) {
+                return BadRequest();          
+            }
+            var student = CollegeRepository.Students.Where(n => n.Id == id).FirstOrDefault();
+            if(student == null)
+            {
+                return NotFound($"The Student With id {id} not Found" );
+            }
+
+            return Ok(student);
+        }
+
+        [HttpGet("{name:alpha}", Name = "GetStudentByName")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+
+        public ActionResult< Student> GetStudentbyname(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return BadRequest();
+            }
+            var student = CollegeRepository.Students.Where(n => n.studentName == name).FirstOrDefault();
+            if (student == null)
+            {
+                return NotFound($"The Student With name {name} not Found");
+            }
+            return Ok(student);
+        }
+
+        [HttpDelete("{id}", Name = "DeleteStudentById")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public ActionResult< bool> DeleteStudent(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+            var student = CollegeRepository.Students.Where(n => n.Id == id).FirstOrDefault();
+            if (student == null)
+            {
+                return NotFound($"The Student With id {id} not Found");
+            }
+            
+            CollegeRepository.Students.Remove(student);
+            return Ok(true);
+        }
+
+    }
+}
---
+++ CollegeApp/Models/CollegeRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace CollegeApp.Models
+{
+    public static class CollegeRepository
+    {
+        public static List<Student> Students { get; set; } = new List<Student>(){
+                new Student
+                {
+                    Id = 1,
+                    studentName = "Zohaib",
+                    Email = "Student1@gmail.com",
+                    Address = "Pakistan",
+                },
+                new Student
+                {
+                    Id = 2,
+                    studentName = "Khan",
+                    Email = "Student2@gmail.com",
+                    Address = "Pakistan",
+                },
+            };
+    }
+}
